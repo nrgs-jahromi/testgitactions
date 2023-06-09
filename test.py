d4309c37806f3955cb4f6e1c568ee36c0df4aa7d
Narges Jahromizadeh <81050996+nrgs-jahromi@users.noreply.github.com>
2023-06-09 10:45:57 +0330

    Create test.py

---
+++ test.py
@@ -0,0 +1,34 @@
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def modulo(x, y):
+    return x % y
+
+def print_result(result):
+    print("Final result:", result)
+
+x = int(input("Enter the first number (x): "))
+y = int(input("Enter the second number (y): "))
+
+
+sum_result = add(x, y)
+print("Sum:", sum_result)
+
+
+difference = subtract(x, y)
+print("Difference:", difference)
+
+
+product = multiply(x, y)
+print("Product:", product)
+
+
+remainder = modulo(x, y)
+print("Remainder:", remainder)
+
